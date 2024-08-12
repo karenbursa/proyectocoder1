c7e3886ac4ebbef0cd97840e9f8ee59218dbbe44
karenbursa <100785136+karenbursa@users.noreply.github.com>
2024-08-12 19:48:35 -0300

    Delete proyecto directory

--- proyecto/productos.js
+++
@@ -1,6 +0,0 @@
-const productos = [
-    { id: 1, nombre: "Agua micelar sephora", Precio: 10200 },
-    { id: 2, nombre: "Crema hidratante garnier", Precio: 7500 },
-    { id: 4, nombre: "Ácido hialurónico", Precio: 9400 },
-    { id: 5, nombre: "Crema anti-age", Precio: 8500 }
-];
